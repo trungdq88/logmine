e533bbfbdf7f71f5f120e22f978f4d682b765d7e
Tony Dinh <trungdq88@gmail.com>
2019-11-03 07:28:13 +0800

    Update test cases

--- src/Clusterer_test.py
+++ src/Clusterer_test.py
@@ -18,21 +18,21 @@ class TestClusterer(unittest.TestCase):
             ]
         )
 
-    # def test_small_max_dist(self):
-    #     clusterer = Clusterer(k1=1, k2=1, max_dist=0.01, variables=[])
-    #     clusters = clusterer.find([
-    #         'hello 1 y 3 ',
-    #         'hello 1 x 3 ',
-    #         'abc m n q ',
-    #     ])
-    #     self.assertEqual(
-    #         clusters,
-    #         [
-    #             [['hello', '1', 'y', '3'], 1],
-    #             [['hello', '1', 'x', '3'], 1],
-    #             [['abc', 'm', 'n', 'q'], 1]
-    #         ]
-    #     )
+    def test_small_max_dist(self):
+        clusterer = Clusterer(k1=1, k2=1, max_dist=0.01, variables=[])
+        clusters = clusterer.find([
+            'hello 1 y 3 ',
+            'hello 1 x 3 ',
+            'abc m n q ',
+        ])
+        self.assertEqual(
+            clusters,
+            [
+                [['hello', '1', 'y', '3'], 1, ['hello', '1', 'y', '3']],
+                [['hello', '1', 'x', '3'], 1, ['hello', '1', 'x', '3']],
+                [['abc', 'm', 'n', 'q'], 1, ['abc', 'm', 'n', 'q']]
+            ]
+        )
 
 
 if __name__ == '__main__':
