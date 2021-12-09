3fa2ffcde65c284af87056c01b03a8e58f7b344d
Tony Dinh <tony@tdinh.me>
2021-12-09 16:03:34 +0700

    Bump version to 0.4.0

--- setup.py
+++ setup.py
@@ -6,7 +6,7 @@ with open("README.md", "r") as fh:
 
 setuptools.setup(
      name='logmine',
-     version='0.3.0',
+     version='0.4.0',
      scripts=['logmine'],
      author="Tony Dinh",
      author_email="pip@tonydinh.com",
